3d61cc1899c8994d6dfc210a145853b88ad4b54c
tatianambmorais <tatianambmorais@gmail.com>
2025-05-12 05:46:27 -0300

    starting project

---
+++ src/test/java/com/example/threatre/ThreatreApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.threatre;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ThreatreApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
